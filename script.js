0d6ded08f9394373e31e95286a5fe2bf408f8684
AsianPsychoBoy <ziyang_99@outlook.com>
2017-02-20 08:00:11 +0800

    refactor main page styles

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 'use strict';
 
+page('/index.html', '/');
+
 page('/', () => {
     render(template('home'));
     const stripeBg = new Stripes();
 })
 
 page('/about', () => {
-    console.log('show about');
-    render('');
+    render(template('about'));
 })
 
 page();
