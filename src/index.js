dbfaf966f529774c4c46f7f03ab65cad77050bf7
AsianPsychoBoy <ziyang_99@outlook.com>
2017-10-14 23:28:28 -0500

    fix2

---
+++ src/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+app.use('/scripts/page', express.static('./node_modules/page/'));
+app.use('/assets', express.static('./src/public/assets'));
+app.use('/', express.static('./src/public'));
+
+app.get('/*', function(req, res) {
+	if (req.path !== '/') {
+		res.redirect('/');
+	}
+})
+
+app.listen(1516, function () {
+  console.log('App listening on port 1516!')
+});
