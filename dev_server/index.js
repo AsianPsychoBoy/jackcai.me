095585f1bb83a72c4ec03388efafa62ae9baef46
AsianPsychoBoy <ziyang_99@outlook.com>
2017-10-14 23:05:45 -0500

    cannot get

--- dev_server/index.js
+++ dev_server/index.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const app = express();
 
-app.use('/scripts/page', express.static('./node_modules/page/'));
-app.use('/assets', express.static('./assets'));
-app.use('/', express.static('./src'));
+// app.use('/scripts/page', express.static('./node_modules/page/'));
+// app.use('/assets', express.static('./assets'));
+// app.use('/', express.static('./src'));
 
-app.all('/*', function(req, res) {
-	console.log('redirected');
-	res.redirect('/');
-})
+// app.get('/*', function(req, res) {
+// 	console.log('redirected');
+// 	res.redirect('/');
+// })
 
 app.listen(1516, function () {
   console.log('App listening on port 1516!')
