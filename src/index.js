bec01cb2cd5a7017d05403640fd715e1c88e1028
AsianPsychoBoy <ziyang_99@outlook.com>
2017-12-15 23:54:10 -0600

    c

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
+const path = require('path');
 const express = require('express');
 const app = express();
 
-app.use('/scripts/page', express.static('./node_modules/page/'));
-app.use('/', express.static('./src/public/'));
+app.use('/scripts/page', express.static(__dirname + '/../node_modules/page/'));
+app.use('/', express.static(__dirname + '/public/'));
 
 app.get('*', function(req, res) {
 	if (req.path !== '/') {
