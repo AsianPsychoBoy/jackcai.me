144ec19a2f76deab11860a5c416772941a4e5ca9
AsianPsychoBoy <ziyang_99@outlook.com>
2017-12-15 23:50:35 -0600

    c

--- src/index.js
+++ src/index.js
@@ -2,11 +2,9 @@ const express = require('express');
 const app = express();
 
 app.use('/scripts/page', express.static('./node_modules/page/'));
-app.use('/assets', express.static('./src/public/assets'));
-app.use('/css', express.static('./src/public/css'));
 app.use('/', express.static('./src/public/'));
 
-app.get('/*', function(req, res) {
+app.get('*', function(req, res) {
 	if (req.path !== '/') {
 		res.redirect('/');
 	}
